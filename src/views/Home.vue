f881b5e5bc4b11f2a4b4988d0358b5f3023277ce
FeryPermana <muhammad.permana@students.amikom.ac.id>
2021-05-12 02:00:56 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+        <HeaderShayna />
+        <HeroSection />  
+        <BannerShayna />  
+        <InstaShayna />  
+        <PartnerShayna />
+        <FooterShayna />
+  </div>
+</template>
+
+<style scoped>
+  .hero-section .hero-items .single-hero-items {
+    background-image: url('/img/hero-1jpg');
+  }
+</style>
+
+<script>
+// @ is an alias to /src
+
+import HeaderShayna from '@/components/HeaderShayna'
+import HeroSection from '@/components/HeroSection'
+import BannerShayna from '@/components/BannerShayna'
+import InstaShayna from '@/components/InstaShayna'
+import PartnerShayna from '@/components/PartnerShayna'
+import FooterShayna from '@/components/FooterShayna'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroSection,
+    BannerShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
